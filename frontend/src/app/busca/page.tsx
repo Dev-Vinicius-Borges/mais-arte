a1d45b80b0a18fb72920f1dd6f6f0126a7cb6185
dev-vinicius-borges <dev.vinicius.borges@outlook.com>
2026-09-19 18:26:17 -0300

    feat: criar componente de página de busca

---
+++ frontend/src/app/busca/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/_components/footer/footer";
+import Navbar from "@/_components/navbar/navbar";
+
+export default function Page() {
+
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
+
+}

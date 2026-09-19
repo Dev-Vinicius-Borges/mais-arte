d750e237c6d68da90eb97fa2b041da58d74158ba
Vinicius <vinicius2teixeira1motter@gmail.com>
2026-09-19 20:17:52 -0300

    fix: ajustar estrutura do componente Navbar para melhor organização e responsividade

--- frontend/src/_components/navbar/navbar.tsx
+++ frontend/src/_components/navbar/navbar.tsx
@@ -2,27 +2,32 @@ import Image from "next/image";
 import logo from "@assets/logo_mais_arte.svg"
 import NavItem from "./navItem";
 import AccountDropdown from "./account-dropdown";
+import Link from "next/dist/client/link";
+import { IoSearchOutline } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <nav className="px-4 py-2.5 bg-surface-inverse">
+    <nav className="px-4 py-2.5 w-full fixed top-0 bg-surface-inverse">
       <div className="m-auto justify-between items-center flex flex-row gap-16 w-11/12">
-        <Image
-          src={logo}
-          alt="Logo"
-          className="aspect-square h-full w-16"
-        />
+        <Link href={`/`}>
+          <Image
+            src={logo}
+            alt="Logo"
+            className="aspect-square h-full w-16"
+          />
+        </Link>
         <span className="gap-9 flex w-full text-text-inverse">
           <NavItem texto="Eventos" link="/" />
           <NavItem texto="Artistas" link="/" />
           <NavItem texto="Espaços" link="/" />
         </span>
 
-        <div>
-          
+        <div className={`flex gap-8 items-center`}>
+          <Link href={`/busca`} >
+            <IoSearchOutline className={`text-text-inverse hover:text-neutral-500 text-xl`} />
+          </Link>
           <AccountDropdown imageUrl="https://picsum.photos/100/100" />
         </div>
-
       </div>
     </nav>
   );

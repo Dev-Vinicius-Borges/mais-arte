3e4a6544c329379f43abc38f60340075d2fa54d0
Vinicius <vinicius2teixeira1motter@gmail.com>
2026-09-19 16:08:57 -0300

    feat: refatorar componente NavItem para aceitar props e atualizar layout da Navbar

--- frontend/src/_components/navbar/navItem.tsx
+++ frontend/src/_components/navbar/navItem.tsx
@@ -1,8 +1,13 @@
-export default function NavItem(){
+import Link from "next/dist/client/link";
 
+export default function NavItem({texto, link}: {texto: string, link: string}) {
+
+  
     return(
 
-        
+        <Link href={link} className="border-3 hover:border-b-text-primary z-1 bottom-0 left-0 w-fit border-transparent gap-9 pb-3 flex items-end text-xl">
+            {texto}
+        </Link>
 
     )
 
--- frontend/src/_components/navbar/navbar.tsx
+++ frontend/src/_components/navbar/navbar.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
 import logo from "@assets/logo_mais_arte.svg"
+import NavItem from "./navItem";
+import AccountDropdown from "./account_dropdown";
 
 export default function Navbar() {
   return (
     <nav className="px-4 py-2.5">
-      <div className="m-auto justify-between w-11/12">
+      <div className="m-auto justify-between items-center flex flex-row gap-16 w-11/12">
         <Image
           src={logo}
           alt="Logo"
           className="aspect-square h-full w-16"
         />
+        <span className="gap-9 flex w-full">
+          <NavItem texto="Eventos" link="/" />
+          <NavItem texto="Artistas" link="/" />
+          <NavItem texto="Espaços" link="/" />
+        </span>
+
+        <AccountDropdown imageUrl="https://picsum.photos/100/100" />
+
       </div>
     </nav>
   );

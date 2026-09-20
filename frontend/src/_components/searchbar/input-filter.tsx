560ba464692b81a9db14caa13db1843079de8853
dev-vinicius-borges <dev.vinicius.borges@outlook.com>
2026-09-20 16:52:59 -0300

    feat: ajustar layout e funcionalidade dos componentes de filtro e busca

--- frontend/src/_components/searchbar/input-filter.tsx
+++ frontend/src/_components/searchbar/input-filter.tsx
@@ -1,22 +1,26 @@
 import { RiArrowDropDownLine } from "react-icons/ri";
 
-interface FilterFieldProps{
+interface FilterFieldProps {
     options: string[];
 };
 
 export default function FilterField(props: FilterFieldProps) {
 
     return (
-        <section className="appearence-none bg-surface-foreground-2 py-2 px-2 rounded-full flex justify-between w-2/12">
-            
-            <select name="filtro" defaultValue="" className="">
-                <option value="" disabled hidden Filtro </option>
-                {props.options.map((element, index) => (<option key={index} values={element}>{element}</option></option>))}
-            </select>
-            <div className="flex items-center">
-                <RiArrowDropDownLine/>
-            </div>
-        </section>
+        <>
+            <section className="appearence-none bg-surface-foreground-2 px-4 p-2 rounded-full flex justify-between w-2/12">
+                <select name="filtro" defaultValue="" className="w-full appearance-none">
+                    {
+                        props.options.map((element, index) => (
+                            <option key={index} value={element}>{element}</option>
+                        ))
+                    }
+                </select>
+                <div className="flex items-center">
+                    <RiArrowDropDownLine width={32}/>
+                </div>
+            </section >
+        </>
     )
 
 }

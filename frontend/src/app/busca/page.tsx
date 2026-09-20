380598b253f5a1a0a75dd21132fd8252818f9d03
dev-vinicius-borges <dev.vinicius.borges@outlook.com>
2026-09-20 16:35:43 -0300

    feat: adicionar componente Card à página de busca

--- frontend/src/app/busca/page.tsx
+++ frontend/src/app/busca/page.tsx
@@ -2,22 +2,32 @@
 
 import Footer from "@/_components/footer/footer";
 import Navbar from "@/_components/navbar/navbar";
+import Card from "@/_components/searchbar/card";
 import SearchField from "@/_components/searchbar/input-search";
-import {useState, type ChangeEvent} from "react";
-import FilterField from "@/_components/searchbar/input-filter"; 
+import { useState, type ChangeEvent } from "react";
+import FilterField from "@/_components/searchbar/input-filter";
 
 export default function Page() {
-    const [ search, setSearch ] = useState<string>("");
+    const [search, setSearch] = useState<string>("");
 
-    const submit = ()=>{};
-    
+    const submit = () => { };
+
     return (
         <main className="bg-surface-inverse">
             <Navbar />
             <div className="mt-20 mb-12 w-11/12 m-auto flex gap-4">
-                <SearchField state={search} change={(e:ChangeEvent<HTMLInputElement>)=> setSearch(e.target.value)} submit={submit}/>
-                <FilterField/>
+                <SearchField state={search} change={(e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} submit={submit} />
+                <FilterField options={["Artistas", "Espaços", "Eventos"]} />
             </div>
+
+            <section className="w-11/12 m-auto grid grid-cols-4 gap-20">
+                {
+                    Array.from({ length: 50 }).map((_, index) => (
+                        <Card key={index}/>
+                    ))
+                }
+            </section>
+
             <Footer />
         </main>
     )
